f934dc3fb2500723681000061440d9ecbb81d0bb
Nate McCall <zznate.m@gmail.com>
2016-09-28 12:51:51 +1300

    Ninja fix for invocation order of core and max pool size.
    Fixes 979af884 for CASSANDRA-12248

--- src/java/org/apache/cassandra/db/compaction/CompactionManager.java
+++ src/java/org/apache/cassandra/db/compaction/CompactionManager.java
@@ -1792,8 +1792,8 @@ public class CompactionManager implements CompactionManagerMBean
 
     public void setConcurrentCompactors(int value)
     {
-        executor.setCorePoolSize(value);
         executor.setMaximumPoolSize(value);
+        executor.setCorePoolSize(value);
     }
 
     public int getCoreCompactorThreads()
